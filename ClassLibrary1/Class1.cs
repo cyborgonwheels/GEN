1a19101106d1224388db56c2947205e577f56adc
Andrew Williams <stuffmanx@gmail.com>
2017-10-09 08:38:10 -0700

    Test Commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Info.Blockchain.API.BlockExplorer;
+
+namespace APIClass
+{
+    public class BTCAddress
+    {
+        String address58 = null;
+
+
+        public BTCAddress(String address)
+        {
+            address58 = address;
+        }
+
+        String getBalance(String address)
+        {
+            GetHashCode();
+
+
+            return "something";
+        }
+    }
+}
