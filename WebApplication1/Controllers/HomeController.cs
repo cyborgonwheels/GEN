d3dcc76590ea54fd81d874f11c3b4b4e0bcbc919
Andrew Williams <stuffmanx@gmail.com>
2017-10-25 17:33:02 -0700

    Displays basic information about a hard coded address on main page.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Info.Blockchain.API.BlockExplorer;
+using Info.Blockchain.API.Client;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +12,21 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
+            string addressString = "1L1BRq7vyf17rsnYCiw9tebY6nmiNTxQgf"; //TODO: add this as a parameter
+
+            var customClient = new BlockchainHttpClient(apiCode: "48461d4b-9e26-43c0-bbe7-875075a6f751");
+
+            BlockExplorer blockExplorer = new BlockExplorer();
+
+            var address = blockExplorer.GetBase58AddressAsync(addressString).Result;
+            
+
+            //TODO: write a loop that creates a list of ints from the transaction list and pass to the ViewData
+            ViewData["Final Balance"] = address.FinalBalance;
+            ViewData["Total Recieved"] = address.TotalReceived;
+            ViewData["Total Sent"] = address.TotalSent;
+
+
             return View();
         }
 
