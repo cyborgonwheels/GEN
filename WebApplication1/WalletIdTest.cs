4c724a5d226e3040f2c0b2cd003771a7e9327d05
Teresa Condon <teresamariecondon@gmail.com>
2017-10-31 19:50:27 -0700

    Update WalletIdTest.cs

--- WebApplication1/WalletIdTest.cs
+++ WebApplication1/WalletIdTest.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Info.Blockchain.API.BlockExplorer;
+using Info.Blockchain.API.Client;
+using Info.Blockchain.API.Models;
 
 // unit test code  
 using System;  
