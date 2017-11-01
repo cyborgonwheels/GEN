3d6ba4ff54ccd0e2c122f8c5ccef83f5768fb14a
Teresa Condon <teresamariecondon@gmail.com>
2017-10-31 19:42:24 -0700

    Update WalletIdTest.cs

--- WebApplication1/WalletIdTest.cs
+++ WebApplication1/WalletIdTest.cs
@@ -1,1 +1,16 @@
 
+// unit test code  
+using System;  
+using Microsoft.VisualStudio.TestTools.UnitTesting;  
+
+namespace BankTests  
+{  
+    [TestClass]  
+    public class BankAccountTests  
+    {  
+        [TestMethod]  
+        public void TestMethod1()  
+        {  
+        }  
+    }  
+}  
