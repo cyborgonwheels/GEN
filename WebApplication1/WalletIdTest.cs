f51faf992a01c9569b4164b08f56b9fa8d76f958
Teresa Condon <teresamariecondon@gmail.com>
2017-11-01 10:25:26 -0700

    Update WalletIdTest.cs

--- WebApplication1/WalletIdTest.cs
+++ WebApplication1/WalletIdTest.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Info.Blockchain.API.BlockExplorer;
 using Info.Blockchain.API.Client;
 using Info.Blockchain.API.Models;
-
+// import the name space that we want you toimport the name space that we want to test
+using BlockchainAnalysisTool
 // unit test code  
 using Microsoft.VisualStudio.TestTools.UnitTesting;  
 
