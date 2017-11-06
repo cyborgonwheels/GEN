6fa721578bcd2f488e06c2e81e5ce3fa339f28db
Jamie Fisher <fisherj18@up.edu>
2017-11-05 19:29:31 -0800

    Testing links, doesn't link to anything right now

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,11 +12,19 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(String adrs = "1L1BRq7vyf17rsnYCiw9tebY6nmiNTxQgf")
+        public IActionResult Index(String adrs)
         {
             if (adrs != null)
             {
-                ViewData["stringAddress"] = adrs; //address parameter
+                String address = adrs;
+                ViewData["stringAddress"] = address; //address parameter
+                //   19TBERtSZYw4V6mXbHpczBgQCYH5MzfM7o
+                //var addressList = new List<String>();
+                //addressList.Add(adrs);
+                //WalletID testwallet = new WalletID(addressList);
+                // WalletID.MASTER_LIST.Add(testwallet);
+                //BlockExplorer blockExplorer = new BlockExplorer();
+                //var address = blockExplorer.GetBase58AddressAsync(adrs).Result;
 
                 //var dataList = new List<List<string>>();
                 //var walletList = WalletID.getRelatedWallets(addressString);
